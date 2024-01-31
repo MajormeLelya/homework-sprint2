a6b85ea853f759a35c0628c6809fb7d649545108
majorlelya <olya-mazhartseva@mail.ru>
2024-01-31 16:34:28 +0300

    hw08 is done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,25 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  switch (action.type) {
+    case "sort":
+      return state.slice().sort((a, b) => {
+        if (action.payload === "up") {
+          return a.name < b.name ? -1 : 1;
+        } else {
+          return a.name > b.name ? -1 : 1;
         }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+      });
+    case "check":
+      return state.filter((e) => e.age >= 18);
+    default:
+      return state;
+  }
+};
